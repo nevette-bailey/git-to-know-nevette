30134025c27798f75a2411983a3ad165436599f0
nevette-bailey <nevettebailey@gmail.com>
2026-03-13 17:24:42 -0400

    Chore: update configs with safe build and typed routes, add detail to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,17 @@ import {MetadataRoute} from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
-        {url: "https://nevettebailey.com", lastModified: new Date()},
-        {url: "https://nevettebailey.com/resume", lastModified: new Date()},
+        {
+            url: "https://nevettebailey.com",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 1,
+        },
+        {
+            url: "https://nevettebailey.com/resume",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.8,
+        },
     ];
 }
